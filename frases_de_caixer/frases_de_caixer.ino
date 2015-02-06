7f1ad73ce602f85f8671429be82360c5f5a9e3e1
Joortbal <jordi_bct@hotmail.com>
2015-02-06 20:03:00 +0100

    Exercicis

---
+++ frases_de_caixer/frases_de_caixer.ino
@@ -0,0 +1,18 @@
+void setup()                // run once, when the sketch starts
+{
+  Serial.begin(9600);       // set up Serial library at 9600 bps
+  Serial.println("Escull el numero de l'operacio que vols realitzar?"); // prints with brake line
+  Serial.println("1. Comprovar numero de targetade credit"); // prints with brake line
+  Serial.println("2. Comprovar numero de compte bancari"); // prints with brake line
+  Serial.println("3. buscar un dijit perdut de la targeta de credit"); // prints with brake line
+}
+void loop()   // run over and over again
+{
+		   // do nothing
+}
+
+
+
+
+
+
